92d78cf22cc7daa835ebdd3cb787b6fa11ff6601
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-05-28 19:19:49 +0300

    Use failsafe globalThis polyfill

--- src/shims/global.js
+++ src/shims/global.js
@@ -1,17 +1,41 @@
-// Returns global object of a current environment.
-export default (() => {
-    if (typeof global !== 'undefined' && global.Math === Math) {
-        return global;
-    }
+// Given that it's a polyfill there are too many warnings to ignore one by one.
+// Polyfill from https://github.com/medikoo/es5-ext/blob/master/global.js.
+/* eslint-disable */
 
-    if (typeof self !== 'undefined' && self.Math === Math) {
-        return self;
-    }
+var naiveFallback = function () {
+    if (typeof self === 'object' && self) return self;
+    if (typeof window === 'object' && window) return window;
+    throw new Error('Unable to resolve global `this`');
+};
 
-    if (typeof window !== 'undefined' && window.Math === Math) {
-        return window;
-    }
+export default (function () {
+    if (this) return this;
 
-    // eslint-disable-next-line no-new-func
-    return Function('return this')();
+    // Unexpected strict mode (may happen if e.g. bundled into ESM module)
+
+    // Fallback to standard globalThis if available
+    if (typeof globalThis === 'object' && globalThis) return globalThis;
+
+    // Thanks @mathiasbynens -> https://mathiasbynens.be/notes/globalthis
+    // In all ES5+ engines global object inherits from Object.prototype
+    // (if you approached one that doesn't please report)
+    try {
+        Object.defineProperty(Object.prototype, '__global__', {
+            get: function () {
+                return this;
+            },
+            configurable: true,
+        });
+    } catch (error) {
+        // Unfortunate case of updates to Object.prototype being restricted
+        // via preventExtensions, seal or freeze
+        return naiveFallback();
+    }
+    try {
+        // Safari case (window.__global__ works, but __global__ does not)
+        if (!__global__) return naiveFallback();
+        return __global__;
+    } finally {
+        delete Object.prototype.__global__;
+    }
 })();
